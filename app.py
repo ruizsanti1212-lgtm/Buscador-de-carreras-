76d9d8ad88156945be3470ae5058bfb00f06a241
ruizsanti1212-lgtm <ruizsanti1212@gmail.com>
2026-07-12 04:21:30 -0400

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-streamlit
-easyocr
-pandas
-pillow
-supabase
+import streamlit as st
+import easyocr
+import pandas as pd
+from PIL import Image
+import os
+import json
+from supabase import create_client, Client
